ff97ccbb5a694f1ecacce68360949d8692233243
Louis Racicot <Louis Racicot>
2016-11-30 17:47:43 -0500

    stuff

--- src/Devoir4.m
+++ src/Devoir4.m
@@ -8,8 +8,8 @@ c_son = (331.3 + 0.606 * h);
 u1 = [1 0;];
 u2 = -1/sqrt(2) * [1 1;];
 
-vt_1 = vT1 * u1;
-vt_2 = vT2 * u2;
+vt_1 = vT1 * u1 * 0.2778;
+vt_2 = vT2 * u2 * 0.2778;
 
 % frequence des sont dans le referentiel des emetteurs (Hz)
 vs1 = 120;
@@ -23,6 +23,10 @@ r_2 = [3000 2000;];
 
 while t <= 101
     t = t + 1;
+    
+    % distance entre les deux trains a l'emission du son
+    d = abs(r_1(t,:)-r_2(t,:));
+    
     %%%%%%%%%%
     %%% 1 etape: On calcul la direction de propagation du signal
     %%% u_1 est la direction du signal tel que recu par le recepteur 1
@@ -56,7 +60,6 @@ while t <= 101
     %%% 3 etape: On calcule l'intensite du son en se servant de la formule
     %%% fournie dans l'enonce
     %%%%%%%%%%
-    
     
     
     %%%%%%%%%%
