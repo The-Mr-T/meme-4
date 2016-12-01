ee581be196f6fd68b75542a3ee561857ee3d7e17
Louis Racicot <Louis Racicot>
2016-11-30 22:27:56 -0500

    finish the goddamn lab

--- src/Devoir4.m
+++ src/Devoir4.m
@@ -1,8 +1,23 @@
 function [nuIT1, nuIT2] = Devoir4(vT1, vT2)
 % DEVOIR4  Calcule la fréquence et l'intensité du son détecté par deux objects en mouvement
-h = 10;
+
+    %%%%%%%%%%
+    %%% 1 etape: On calcul la direction de propagation du signal
+    %%% u_1 est la direction du signal tel que recu par le recepteur 1
+    %%% (emit par le train 2). Formule page 60, ch07.
+    %%%%%%%%%%
+        
+    %%%%%%%%%%
+    %%% 2 etape: On veut calculer v_1, la frequence de l'onde dans le
+    %%% selon le recepteur 1. Formule par 61, ch07.
+    %%%%%%%%%%
+    
+    %%%%%%%%%%
+    %%% 3 etape: On calcule l'intensite du son en se servant de la formule
+    %%% fournie dans l'enonce
+    %%%%%%%%%%
+        
 t = 0;
-c_son = (331.3 + 0.606 * h);
 
 % calcul des vitesses vectorielles
 u1 = [1 0;];
@@ -10,10 +25,6 @@ u2 = -1/sqrt(2) * [1 1;];
 
 vt_1 = vT1 * u1 * 0.2778;
 vt_2 = vT2 * u2 * 0.2778;
-
-% frequence des sont dans le referentiel des emetteurs (Hz)
-vs1 = 120;
-vs2 = 170;
 
 % Position du train 1 au temps 0
 r_1 = [0 0;];
@@ -23,44 +34,21 @@ r_2 = [3000 2000;];
 
 while t <= 101
     t = t + 1;
+    deltat = 0;
+    nuIT1(t,:) = [0 0;];
+    nuIT2(t,:) = [0 0;];
     
-    % distance entre les deux trains a l'emission du son
-    d = abs(r_1(t,:)-r_2(t,:));
+    while t - deltat > 0 && nuIT1(t,1) == 0 && t - deltat < 80
+        nuIT1(t,:) = getSoundInfo(r_1, r_2, t, deltat, vt_1, vt_2, 170);
+        deltat = deltat + 1;
+    end
     
-    %%%%%%%%%%
-    %%% 1 etape: On calcul la direction de propagation du signal
-    %%% u_1 est la direction du signal tel que recu par le recepteur 1
-    %%% (emit par le train 2). Formule page 60, ch07.
-    %%%%%%%%%%
+    deltat = 0;
     
-    % recepteur 1
-    u_1 = (r_1(t,:)-r_2(t,:))/norm(r_1(t,:)-r_2(t,:))
-    
-    % recepteur 2
-    u_2 = (r_2(t,:)-r_1(t,:))/norm(r_2(t,:)-r_1(t,:));
-    
-    
-    %%%%%%%%%%
-    %%% 2 etape: On veut calculer v_1, la frequence de l'onde dans le
-    %%% selon le recepteur 1. Formule par 61, ch07.
-    %%%%%%%%%%
-    
-    % recepteur 1
-    b_r1 = dot(vt_1, u_1) / c_son;
-    b_s1 = dot(vt_2, u_1) / c_son;
-    nuIT1(t,1) = (1-b_r1)/(1-b_s1) * vs2
-    
-    % recepteur 2
-    b_r2 = dot(vt_2, u_2) / c_son;
-    b_s2 = dot(vt_1, u_2) / c_son;
-    nuIT2(t,1) = (1-b_r2)/(1-b_s2) * vs1
-    
-    
-    %%%%%%%%%%
-    %%% 3 etape: On calcule l'intensite du son en se servant de la formule
-    %%% fournie dans l'enonce
-    %%%%%%%%%%
-    
+    while t - deltat > 0 && nuIT2(t,1) == 0 && t - deltat < 80
+        nuIT2(t,:) = getSoundInfo(r_2, r_1, t, deltat, vt_2, vt_1, 120);
+        deltat = deltat + 1;
+    end
     
     %%%%%%%%%%
     %%% 4 etape: On fait avancer les trains
